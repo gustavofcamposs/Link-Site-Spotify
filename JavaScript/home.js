9552db028c77d4d48dbdfc6d10ce1a0330418ddb
gustavofcampos2 <gustavofcamposs534@gmail.com>
2024-01-20 21:48:15 -0300

    teste2

--- JavaScript/home.js
+++
@@ -1,7 +0,0 @@
-function clickMenu(){ 
-    if (menu_Hamburguer.style.display == 'block'){ 
-        menu_Hamburguer.style.display = 'none'
-    } else{
-        menu_Hamburguer.style.display = 'block'
-    }
-}
